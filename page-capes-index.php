2bf9aa5642c4ad1352592bd0d5f0905e9d5bf9ec
wilkesvb <vincentwilkes@MacBook-Pro.local>
2016-05-03 13:27:35 -0400

    More Partials

--- page-capes-index.php
+++ page-capes-index.php
@@ -24,34 +24,7 @@
 				
 				<?php endif; ?>
 				
-				<?php 
-
-					$args = array(
-						'post_type' => 'capabilitysummary'
-					);
-					$query = new WP_Query( $args );
-
-				?>
-
-				<?php if( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-
-
-				<div class="capes col-1-3">
-
-					<a href="<?php the_permalink(); ?>"><?php the_field( 'capability_thumbnail' ); ?></a>
-
-					<div class="container">
-
-						<h4><?php the_field( 'capability_name' ); ?></h4>
-
-						<p><?php the_field( 'capability_summary'); ?></p>
-						
-						<div class="capes-button"><a href="<?php the_field( 'capability_link' ) ?>">read more</a></div>
-					</div>
-
-				</div>
-
-				<?php endwhile; endif; wp_reset_postdata(); ?>
+				<?php get_template_part ( '/partials/part' , 'capes-summary' ); ?>
 
 			</div>
 
